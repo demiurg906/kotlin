333421340808b838a362ef312f71b468b58114c6
Dmitriy Novozhilov <novodmit.2x@yandex.ru>
2018-08-15 14:17:50 +0300

    `RENDER_DIAGNOSTICS_MESSAGES` directive added

--- compiler/tests-common/tests/org/jetbrains/kotlin/checkers/BaseDiagnosticsTest.kt
+++ compiler/tests-common/tests/org/jetbrains/kotlin/checkers/BaseDiagnosticsTest.kt
@@ -139,6 +139,7 @@ abstract class BaseDiagnosticsTest : KotlinMultiFileTestWithJava<TestModule, Tes
         val dynamicCallDescriptors: List<DeclarationDescriptor> = ArrayList()
         val withNewInferenceDirective: Boolean
         val newInferenceEnabled: Boolean
+        val renderDiagnosticMessages: Boolean
 
         init {
             this.declareCheckType = CHECK_TYPE_DIRECTIVE in directives
@@ -161,6 +162,7 @@ abstract class BaseDiagnosticsTest : KotlinMultiFileTestWithJava<TestModule, Tes
                 this.clearText = CheckerTestUtil.parseDiagnosedRanges(addExtras(expectedText), diagnosedRanges)
                 this.createKtFile = lazy { TestCheckerUtil.createCheckAndReturnPsiFile(fileName, clearText, project) }
             }
+            this.renderDiagnosticMessages = RENDER_DIAGNOSTICS_MESSAGES in directives
         }
 
         val ktFile: KtFile? by createKtFile
@@ -299,7 +301,8 @@ abstract class BaseDiagnosticsTest : KotlinMultiFileTestWithJava<TestModule, Tes
                     diagnosticToExpectedDiagnostic,
                     { file -> file.text },
                     uncheckedDiagnostics,
-                    withNewInferenceDirective
+                    withNewInferenceDirective,
+                    renderDiagnosticMessages
                 )
             )
 
@@ -371,6 +374,8 @@ abstract class BaseDiagnosticsTest : KotlinMultiFileTestWithJava<TestModule, Tes
 
         // Change it to "true" to load diagnostics for old inference to test new inference (ignore diagnostics with <NI; prefix)
         val USE_OLD_INFERENCE_DIAGNOSTICS_FOR_NI = false
+
+        val RENDER_DIAGNOSTICS_MESSAGES = "RENDER_DIAGNOSTICS_MESSAGES"
 
         private fun parseDiagnosticFilterDirective(
             directiveMap: Map<String, String>,
