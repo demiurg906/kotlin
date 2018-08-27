ec7442f200a8f58c9c1a70bdda4be38de071fa86
Dmitriy Novozhilov <novodmit.2x@yandex.ru>
2018-08-27 14:28:58 +0300

    Fix logic of `combine` method on call facts

--- compiler/contextual-effects/src/org/jetbrains/kotlin/contracts/safebuilders/CallCombiner.kt
+++ compiler/contextual-effects/src/org/jetbrains/kotlin/contracts/safebuilders/CallCombiner.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.kotlin.contracts.safebuilders
 
 import org.jetbrains.kotlin.contracts.description.InvocationKind
+import org.jetbrains.kotlin.contracts.description.InvocationKind.*
 import org.jetbrains.kotlin.contracts.facts.Context
 import org.jetbrains.kotlin.contracts.facts.ContextFact
 import org.jetbrains.kotlin.contracts.facts.FactsCombiner
@@ -15,13 +16,17 @@ object CallCombiner : FactsCombiner() {
         if (a !is CallContext || b !is CallContext) throw AssertionError()
 
         val functions = a.calls.keys union b.calls.keys
-        val updatedCalls = functions.map { functionReference ->
+        val updatedCalls = functions.mapNotNull { functionReference ->
             val aFact = a.calls[functionReference]
             val bFact = b.calls[functionReference]
 
             val aKind = aFact?.kind ?: InvocationKind.ZERO
             val bKind = bFact?.kind ?: InvocationKind.ZERO
             val resKind = or(aKind, bKind)
+
+            if (resKind == ZERO) {
+                return@mapNotNull null
+            }
 
             val calledElement = aFact?.calledElement ?: bFact?.calledElement ?: throw AssertionError()
 
@@ -56,79 +61,74 @@ object CallCombiner : FactsCombiner() {
     }
 
     private fun or(x: InvocationKind, y: InvocationKind) = when (x) {
-        InvocationKind.ZERO -> when (y) {
-            InvocationKind.ZERO -> InvocationKind.ZERO
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.UNKNOWN
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        ZERO -> when (y) {
+            ZERO -> ZERO
+            AT_MOST_ONCE -> AT_MOST_ONCE
+            EXACTLY_ONCE -> AT_MOST_ONCE
+            AT_LEAST_ONCE -> UNKNOWN
+            UNKNOWN -> UNKNOWN
         }
-        InvocationKind.AT_MOST_ONCE -> when (y) {
-            InvocationKind.ZERO -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.UNKNOWN
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        AT_MOST_ONCE -> when (y) {
+            ZERO -> AT_MOST_ONCE
+            AT_MOST_ONCE -> AT_MOST_ONCE
+            EXACTLY_ONCE -> AT_MOST_ONCE
+            AT_LEAST_ONCE -> UNKNOWN
+            UNKNOWN -> UNKNOWN
         }
-        InvocationKind.EXACTLY_ONCE -> when (y) {
-            InvocationKind.ZERO -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.EXACTLY_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        EXACTLY_ONCE -> when (y) {
+            ZERO -> AT_MOST_ONCE
+            AT_MOST_ONCE -> AT_MOST_ONCE
+            EXACTLY_ONCE -> EXACTLY_ONCE
+            AT_LEAST_ONCE -> AT_LEAST_ONCE
+            UNKNOWN -> UNKNOWN
         }
-        InvocationKind.AT_LEAST_ONCE -> when (y) {
-            InvocationKind.ZERO -> InvocationKind.UNKNOWN
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.UNKNOWN
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        AT_LEAST_ONCE -> when (y) {
+            ZERO -> UNKNOWN
+            AT_MOST_ONCE -> UNKNOWN
+            EXACTLY_ONCE -> AT_LEAST_ONCE
+            AT_LEAST_ONCE -> AT_LEAST_ONCE
+            UNKNOWN -> UNKNOWN
         }
-        InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        UNKNOWN -> UNKNOWN
     }
 
     private fun combine(x: InvocationKind, y: InvocationKind) = when (x) {
-        InvocationKind.ZERO -> when (y) {
-            InvocationKind.ZERO -> InvocationKind.ZERO
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.EXACTLY_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        ZERO -> when (y) {
+            ZERO -> ZERO
+            AT_MOST_ONCE -> AT_MOST_ONCE
+            EXACTLY_ONCE -> EXACTLY_ONCE
+            AT_LEAST_ONCE -> AT_LEAST_ONCE
+            UNKNOWN -> UNKNOWN
         }
-        InvocationKind.AT_MOST_ONCE -> when (y) {
-            InvocationKind.ZERO -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.UNKNOWN
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        AT_MOST_ONCE -> when (y) {
+            ZERO -> AT_MOST_ONCE
+            AT_MOST_ONCE -> UNKNOWN
+            EXACTLY_ONCE -> AT_LEAST_ONCE
+            AT_LEAST_ONCE -> AT_LEAST_ONCE
+            UNKNOWN -> UNKNOWN
         }
-        InvocationKind.EXACTLY_ONCE -> when (y) {
-            InvocationKind.ZERO -> InvocationKind.EXACTLY_ONCE
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        EXACTLY_ONCE -> when (y) {
+            ZERO -> EXACTLY_ONCE
+            else -> AT_LEAST_ONCE
         }
-        InvocationKind.AT_LEAST_ONCE -> if (y == InvocationKind.UNKNOWN) InvocationKind.UNKNOWN else InvocationKind.AT_LEAST_ONCE
-        InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        AT_LEAST_ONCE -> AT_LEAST_ONCE
+        UNKNOWN -> when (y) {
+            EXACTLY_ONCE -> AT_LEAST_ONCE
+            AT_LEAST_ONCE -> AT_LEAST_ONCE
+            else -> UNKNOWN
+        }
     }
 
     private fun updateWithCallKind(kind: InvocationKind, functionInvocationKind: InvocationKind) = when (functionInvocationKind) {
-        InvocationKind.AT_MOST_ONCE -> when (kind) {
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.AT_MOST_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.UNKNOWN
-            InvocationKind.ZERO -> InvocationKind.ZERO
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
+        AT_MOST_ONCE -> when (kind) {
+            AT_MOST_ONCE -> AT_MOST_ONCE
+            EXACTLY_ONCE -> AT_MOST_ONCE
+            AT_LEAST_ONCE -> UNKNOWN
+            ZERO -> ZERO
+            UNKNOWN -> UNKNOWN
         }
-        InvocationKind.AT_LEAST_ONCE -> when (kind) {
-            InvocationKind.AT_MOST_ONCE -> InvocationKind.UNKNOWN
-            InvocationKind.EXACTLY_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.AT_LEAST_ONCE -> InvocationKind.AT_LEAST_ONCE
-            InvocationKind.ZERO -> InvocationKind.ZERO
-            InvocationKind.UNKNOWN -> InvocationKind.UNKNOWN
-        }
-        InvocationKind.EXACTLY_ONCE -> kind
+        AT_LEAST_ONCE -> kind
+        EXACTLY_ONCE -> kind
         else -> throw AssertionError()
     }
 }
